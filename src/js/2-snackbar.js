ca92abc352278c9cb1a63629d146b436ab2430f0
LesyaKot <lesyakot83@gmail.com>
2024-02-14 14:52:26 +0500

    add html js

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,8 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+const button = document.querySelector('button');
+
+
+
